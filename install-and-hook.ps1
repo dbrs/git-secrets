387dd979adfbad19fb0c1c69fa891a55b0b43055
Sumeet Mankoo <smankoo@dbrs.com>
2019-05-22 11:40:20 -0400

    installation script now generates logs in %TEMP%

--- install-and-hook.ps1
+++ install-and-hook.ps1
@@ -1,3 +1,4 @@
-.\install-secrets.ps1
-.\configure-hooks-template.ps1
-.\configure-on-existing-repos.ps1
+ts=Get-Date -Format o | foreach {$_ -replace ":", "."}
+.\install-secrets.ps1 | Tee-Object -FilePath "$env:TEMP\install-and-hook.$ts.log"
+.\configure-hooks-template.ps1 | Tee-Object -FilePath "$env:TEMP\install-and-hook.$ts.log"
+.\configure-on-existing-repos.ps1 | Tee-Object -FilePath "$env:TEMP\install-and-hook.$ts.log"
