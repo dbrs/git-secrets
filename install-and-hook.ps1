15a87d8ea94d591ec02ec851ea54d287d96b6a72
Sumeet Mankoo <smankoo@dbrs.com>
2019-05-23 15:11:06 -0400

    fixed issue with installer paths

--- install-and-hook.ps1
+++ install-and-hook.ps1
@@ -1,4 +1,4 @@
 $ts=Get-Date -Format o | ForEach-Object {$_ -replace ":", "."}
-&"install-secrets.ps1" | Tee-Object -FilePath "$env:TEMP\install-and-hook.$ts.log"
-&"configure-hooks-template.ps1" | Tee-Object -FilePath "$env:TEMP\install-and-hook.$ts.log"
-&"configure-on-existing-repos.ps1" | Tee-Object -FilePath "$env:TEMP\install-and-hook.$ts.log"
+&".\install-secrets.ps1" | Tee-Object -FilePath "$env:TEMP\install-and-hook.$ts.log"
+&".\configure-hooks-template.ps1" | Tee-Object -FilePath "$env:TEMP\install-and-hook.$ts.log"
+&".\configure-on-existing-repos.ps1" | Tee-Object -FilePath "$env:TEMP\install-and-hook.$ts.log"
