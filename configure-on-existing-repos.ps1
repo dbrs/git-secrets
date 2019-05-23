8420286178a7e8010ea597e20577cf477193c0f6
Sumeet Mankoo <smankoo@dbrs.com>
2019-05-23 16:39:26 -0400

    fixed bug with installation on redirected folders

--- configure-on-existing-repos.ps1
+++ configure-on-existing-repos.ps1
@@ -2,7 +2,7 @@
 GET-WMIOBJECT -query "SELECT * from win32_logicaldisk where DriveType = '3'" | ForEach-Object {
     $driveLetter=$_.DeviceID
 
-    Write-Output = "$driveLetter"
+    Write-Output = "Scanning Drive: " + $driveLetter
     Get-ChildItem -Path $driveLetter\ -Filter .git -Recurse -ErrorAction SilentlyContinue -Force | Select-Object @{ n = 'Folder'; e = { Convert-Path $_.PSParentPath } } | ForEach-Object {
         $repoDir=$_.Folder
         # Go to the repository Folder
@@ -16,17 +16,20 @@ GET-WMIOBJECT -query "SELECT * from win32_logicaldisk where DriveType = '3'" | F
 
 # Also install for redirected folders
 [Environment+SpecialFolder]::GetNames([Environment+SpecialFolder]) | ForEach-Object {
-    [Environment]::GetFolderPath($_) | Select-String -Pattern '^(?![C:|D:].+$).*' | Select-String -Pattern '.+'
-    $folder=$_
+    [Environment]::GetFolderPath($_) | Select-String -Pattern '^(?![C:|D:].+$).*' | Select-String -Pattern '.+' | ForEach-Object {
+        Write-Host "Scanning Folder: " + $_
 
-    Get-ChildItem -Path $folder\ -Filter .git -Recurse -ErrorAction SilentlyContinue -Force | Select-Object @{ n = 'Folder'; e = { Convert-Path $_.PSParentPath } } | ForEach-Object {
-        $repoDir=$_.Folder
-        # Go to the repository Folder
-        Set-Location -Path $repoDir
-        
-        Write-Output "Installing hook in repo: $repoDir"
-        git secrets --install
-        git secrets --register-aws
+        $folder=$_
+
+        Get-ChildItem -Path $folder\ -Filter .git -Recurse -ErrorAction SilentlyContinue -Force | Select-Object @{ n = 'Folder'; e = { Convert-Path $_.PSParentPath } } | ForEach-Object {
+            $repoDir=$_.Folder
+            Go to the repository Folder
+            Set-Location -Path $repoDir
+            
+            Write-Output "Installing hook in repo: $repoDir"
+            git secrets --install
+            git secrets --register-aws
+        }
     }
 }
 
