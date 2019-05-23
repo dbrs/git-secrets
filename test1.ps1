885650072721cca575d8c88179f6a7dab051fbf2
Sumeet Mankoo <smankoo@dbrs.com>
2019-05-23 18:02:16 -0400

    removed test script

--- test1.ps1
+++
@@ -1,5 +0,0 @@
-[Environment+SpecialFolder]::GetNames([Environment+SpecialFolder]) | ForEach-Object {
-    [Environment]::GetFolderPath($_) | Select-String -Pattern '^(?!.:.+$).*' | Select-String -Pattern '.+' | ForEach-Object {
-        Write-Host "Scanning Folder: " $_
-    }
-}
